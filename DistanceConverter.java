9b2cebb945c423e304b37cd091ddaed41099ea46
aditya041003 <greatgamer0430@gmail.com>
2025-06-13 10:15:44 +0530

    initial commit

---
+++ DistanceConverter.java
@@ -0,0 +1,7 @@
+public class DistanceConverter {
+    public static void main(String[] args) {
+        double km = 10.8; // Example distance in miles
+        double miles = km * 1.6; // Convert miles to kilometers
+        System.out.println("The distance " +km+" in kilometers is: " + miles);
+    }
+}
---
+++ MaxHandshakes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MaxHandshakes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numberOfStudents;
+
+        System.out.print("Enter number of students: ");
+        numberOfStudents = input.nextInt();
+
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+        System.out.println("The maximum number of possible handshakes is " + handshakes);
+    }
+}
